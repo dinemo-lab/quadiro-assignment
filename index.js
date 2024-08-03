6e45cba1628c8af701b685cca3022c1d50ed323c
Dinesh Maurya <dineshmourya02@gmail.com>
2024-08-03 20:40:19 +0530

    change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', require('./src/routes/auth'));
-app.use('/api/cars', require('./src/routes/car'));
+app.use('/api/cars', require('./src/routes/Car'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
